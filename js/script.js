2ec7241405f70ac7a524337a62729aead861ab40
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-14 21:09:24 +0200

    v.0.8.0 (fixed timer, cards and sidepanel)

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import modal from './modules/modal';
 import timer from './modules/timer';
 import cards from './modules/cards';
 import calc from './modules/calc';
+import forms from './modules/forms';
 import slider from './modules/slider';
 import {openModal} from './modules/modal';
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
